56ebfbea485f0882c87788a9ed37f839190a3fe3
Kim-yongbeom <subum12345@gmail.com>
2021-10-12 17:11:24 +0900

    진행중

--- wvl_frontend/src/containers/auth/SignInForm.js
+++ wvl_frontend/src/containers/auth/SignInForm.js
@@ -4,62 +4,66 @@ import { useHistory } from "react-router-dom";
 import AuthForm from "../../components/auth/AuthForm";
 import AuthContext from "../../context/AuthContext";
 import client from "../../libs/api/_client";
+import {
+  ToastsContainer,
+  ToastsStore,
+  ToastsContainerPosition,
+} from "react-toasts";
 
 function SignInForm() {
-    const history = useHistory();
+  const history = useHistory();
 
-    const { authInfo, setAuthInfo } = useContext(AuthContext);
+  const { authInfo, setAuthInfo } = useContext(AuthContext);
 
-    const [error, setError] = useState("");
-    const [form, setForm] = useState({
-        email: "",
-        password: "",
+  const [error, setError] = useState("");
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const onChangeInput = (event) => {
+    const { name, value } = event.target;
+    setForm({
+      ...form,
+      [name]: value,
     });
+  };
 
-    const onChangeInput = (event) => {
-        const { name, value } = event.target;
-        setForm({
-            ...form,
-            [name]: value,
-        });
-    };
+  const onClickSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await client.post("/vaccine/auth/signin", {
+        email: form.email,
+        password: form.password,
+      });
+      console.log(response);
+      if (response.status === 200) {
+        const accessToken = response.data.accessToken;
+        localStorage.setItem("accessToken", accessToken);
+        client.defaults.headers.common["Authorization"] = `${accessToken}`;
+        const result = await client.get("/vaccine/auth/profile");
+        console.log(result.data.data);
+        setAuthInfo({ isLoggedIn: true, userInfo: result.data.data });
+        ToastsStore.success("회원가입 완료");
+        history.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+      if (error.response.status === 400) {
+        setError("이메일 / 비밀번호를 확인해 주시기 바랍니다.");
+      }
+    }
+  };
 
-    const onClickSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const response = await client.post("/vaccine/auth/signin", {
-                email: form.email,
-                password: form.password,
-            });
-            console.log(response);
-            if (response.status === 200) {
-                const accessToken = response.data.accessToken;
-                localStorage.setItem("accessToken", accessToken);
-                client.defaults.headers.common[
-                    "Authorization"
-                ] = `${accessToken}`;
-                const result = await client.get("/vaccine/auth/profile");
-                console.log(result.data.data);
-                setAuthInfo({ isLoggedIn: true, userInfo: result.data.data });
-                history.push("/");
-            }
-        } catch (error) {
-            console.log(error);
-            if (error.response.status === 400) {
-                setError("이메일 / 비밀번호를 확인해 주시기 바랍니다.");
-            }
-        }
-    };
-
-    return (
-        <AuthForm
-            type="login"
-            onClickSubmit={onClickSubmit}
-            form={form}
-            onChangeInput={onChangeInput}
-            error={error}
-        />
-    );
+  return (
+    <AuthForm
+      type="login"
+      onClickSubmit={onClickSubmit}
+      form={form}
+      onChangeInput={onChangeInput}
+      error={error}
+    />
+  );
 }
 
 export default SignInForm;
--- wvl_frontend/src/containers/auth/SignUpForm.js
+++ wvl_frontend/src/containers/auth/SignUpForm.js
@@ -3,90 +3,93 @@ import { useState } from "react";
 import { useHistory } from "react-router";
 import AuthForm from "../../components/auth/AuthForm";
 import client from "../../libs/api/_client";
+import {
+  ToastsContainer,
+  ToastsStore,
+  ToastsContainerPosition,
+} from "react-toasts";
 
 function SignUpForm() {
-    const history = useHistory();
+  const history = useHistory();
 
-    const [error, setError] = useState("");
-    const [form, setForm] = useState({
-        email: "",
-        password: "",
-        passwordConfirm: "",
-        nickName: "",
+  const [error, setError] = useState("");
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+    passwordConfirm: "",
+    nickName: "",
+  });
+
+  const onChangeInput = (event) => {
+    const { name, value } = event.target;
+    setForm({
+      ...form,
+      [name]: value,
     });
 
-    const onChangeInput = (event) => {
-        const { name, value } = event.target;
-        setForm({
-            ...form,
-            [name]: value,
+    if (name === "passwordConfirm") {
+      if (form.password === value && form.password.length > 0) {
+        setError("비밀번호가 일치합니다.");
+      } else {
+        setError("비밀번호가 서로 다릅니다, 다시 입력해주세요.");
+      }
+    }
+
+    if (name === "password") {
+      const reg = /^(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
+
+      if (form.passwordConfirm === value && form.passwordConfirm.length > 0) {
+        setError("비밀번호가 일치합니다.");
+      } else {
+        if (!reg.test(value)) {
+          setError(
+            "비밀번호는 8자 이상이어야 하며, 숫자/소문자/특수문자를 모두 포함해야 합니다."
+          );
+          return;
+        }
+        setError("비밀번호가 서로 다릅니다, 다시 입력해주세요.");
+      }
+    }
+  };
+
+  const onClickSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      if (form.password !== form.passwordConfirm) {
+        setError("비밀번호가 서로 다릅니다, 다시 입력해주세요.");
+      } else {
+        const response = await client.post("/vaccine/auth/signup", {
+          email: form.email,
+          nickName: form.nickName,
+          password: form.passwordConfirm,
         });
 
-        if (name === "passwordConfirm") {
-            if (form.password === value && form.password.length > 0) {
-                setError("비밀번호가 일치합니다.");
-            } else {
-                setError("비밀번호가 서로 다릅니다, 다시 입력해주세요.");
-            }
+        if (response.status === 200) {
+          console.log("회원가입 성공");
+          ToastsStore.success("회원가입 완료");
+          history.goBack();
         }
+      }
+    } catch (error) {
+      const errorStatus = error.response.status;
+      if (errorStatus === 400) {
+        setError("중복된 이메일 혹은 닉네임 존재합니다.");
+      } else {
+        setError("올바르게 입력해주세요.");
+      }
+    }
+  };
 
-        if (name === "password") {
-            const reg = /^(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
-
-            if (
-                form.passwordConfirm === value &&
-                form.passwordConfirm.length > 0
-            ) {
-                setError("비밀번호가 일치합니다.");
-            } else {
-                if (!reg.test(value)) {
-                    setError(
-                        "비밀번호는 8자 이상이어야 하며, 숫자/소문자/특수문자를 모두 포함해야 합니다."
-                    );
-                    return;
-                }
-                setError("비밀번호가 서로 다릅니다, 다시 입력해주세요.");
-            }
-        }
-    };
-
-    const onClickSubmit = async (e) => {
-        e.preventDefault();
-
-        try {
-            if (form.password !== form.passwordConfirm) {
-                setError("비밀번호가 서로 다릅니다, 다시 입력해주세요.");
-            } else {
-                const response = await client.post("/vaccine/auth/signup", {
-                    email: form.email,
-                    nickName: form.nickName,
-                    password: form.passwordConfirm,
-                });
-
-                if (response.status === 200) {
-                    console.log("회원가입 성공");
-                    history.goBack();
-                }
-            }
-        } catch (error) {
-            const errorStatus = error.response.status;
-            if (errorStatus === 400) {
-                setError("중복된 이메일 혹은 닉네임 존재합니다.");
-            } else {
-                setError("올바르게 입력해주세요.");
-            }
-        }
-    };
-
-    return (
-        <AuthForm
-            onClickSubmit={onClickSubmit}
-            onChangeInput={onChangeInput}
-            type="register"
-            error={error}
-            form={form}
-        />
-    );
+  return (
+    <AuthForm
+      onClickSubmit={onClickSubmit}
+      onChangeInput={onChangeInput}
+      type="register"
+      error={error}
+      form={form}
+    />
+  );
 }
 
 export default SignUpForm;
